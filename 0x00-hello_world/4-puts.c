f0cd4580769370a2e6a98c4cfecb3505e4217cfa
salma-asar <salma.asar5510@gmail.com>
2023-12-19 19:50:16 -0800

    putsc

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ *  return always 0 (sucsess) 
+*/   
+
+ int main()
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}    
