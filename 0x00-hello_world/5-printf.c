3005897beae18ff043a4c3a6785f53b09d93baac
salma-asar <salma.asar5510@gmail.com>
2023-12-20 07:41:33 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Iam a C programer.\n");
+    printf("I create art with proper grammar,\n");
+    printf("but the outcome is a piece of art.\n");
+
+    return 0;
+}             
